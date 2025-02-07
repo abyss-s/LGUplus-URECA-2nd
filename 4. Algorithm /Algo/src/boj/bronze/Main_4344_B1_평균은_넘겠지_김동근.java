3248a68774c6ec51cee3007c5ee367e524d7cf9a
abyss-s <lh3729@naver.com>
2025-02-07 15:49:30 +0900

    [02/06] algorithm - init setting

---
+++ 4. Algorithm /Algo/src/boj/bronze/Main_4344_B1_평균은_넘겠지_김동근.java
@@ -0,0 +1,18 @@
+package boj.bronze;
+
+public class Main_4344_B1_평균은_넘겠지_김동근 {
+
+	public static void main(String[] args) throws Exception {
+		// input
+
+		// average 계산
+
+		// 학생 성적이 평균을 넘는지 체크하고 count 세기
+
+		// 비율 구하기 count /N
+
+		// output - 소수 셋째자리까지 출력 (반올림)
+
+	}
+
+}
