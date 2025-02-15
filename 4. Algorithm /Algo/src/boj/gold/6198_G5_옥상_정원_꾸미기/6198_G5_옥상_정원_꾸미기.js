72b8b5e024062d0d96310f9787521c1679a3e97e
abyss-s <lh3729@naver.com>
2025-02-16 01:03:00 +0900

    algo: 백준 6198번 옥상정원 꾸미기

---
+++ 4. Algorithm /Algo/src/boj/gold/6198_G5_옥상_정원_꾸미기/6198_G5_옥상_정원_꾸미기.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+// 백준 제출용
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+// 로컬 테스트용
+// const filePath =
+//   process.platform === 'linux' ? 'dev/stdin' : __dirname + '/input.txt';
+// let input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+// 6198번 옥상 정원 꾸미기
+let n = Number(input.shift());
+let buildings = input.map(Number);
+let stack = [];
+let count = 0;
+
+for (let i = 0; i < n; i++) {
+  while (stack.length > 0) {
+    if (stack[stack.length - 1] <= buildings[i]) stack.pop();
+    else break;
+  }
+
+  count += stack.length;
+
+  stack.push(buildings[i]);
+}
+
+console.log(count);
