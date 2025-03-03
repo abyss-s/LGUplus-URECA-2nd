ea194c27d2df7cc9b19213e0af38e582fb795d65
abyss-s <lh3729@naver.com>
2025-03-03 22:35:46 +0900

    [02/28] SQL - group by, join 연습

--- 5. Backend/0228/emp_group.sql
+++ 5. Backend/0228/emp_group.sql
@@ -1,18 +1,36 @@
 -- 부서별   사원수,  급여 평균, 커미션을 받는 사원 수를 조회 하시오
-
+select  deptno, count(*), avg(sal), count(comm)
+from emp
+group by deptno;
 
 -- 업무별   평균 급여, 최저 급여, 최고 급여를 조회 하시오
+select job, avg(sal), min(sal), max(sal) 
+from emp
+group by job;
 
 -- 부서별, 업무별 통계  : 사원수,  급여 평균, 커미션을 받는 사원 수를 조회 하시오
-
+select deptno, job, count(*), avg(sal), count(comm)
+from emp
+group by deptno, job;
 
 -- 부서별 총 급여가 10000이하인 부서의 총 사원수, 총 급여를 조회
+select deptno, count(*), sum(sal) as totalSum 
+from emp
+group by deptno having totalSum <= 10000;
 
 -- 부서별 총 급여를 조회,  급여가 1000이상인 사원들의 급여 합계를 구함.
-
+select deptno, count(*), sum(sal) as totalSum
+from emp
+where sal >= 1000
+group by deptno order by deptno;
 
 -- 상품별 수량이 10개 초과인 제품에 대해 상품별 총 판매 수량 조회
+select gno, count(*), sum(quantity) as orders
+from orders
+group by gno having orders > 10;
 
 -- 건당 판매 수량이 2개 이상인 판매 건수를 상품별 조회
-
-
+select gno, count(quantity) as orders
+from ORDERS
+where quantity >= 2
+group by gno;
--- 5. Backend/0228/emp_join.sql
+++ 5. Backend/0228/emp_join.sql
@@ -1,33 +1,72 @@
 -- 1. CLERK 업무를 담당하고 급여 1000보다 많이 받는 사원의
 --    사원번호, 이름, 급여, 부서명을 조회하시오
-
+select empno, ename, sal, dname, job
+from emp e
+join dept d
+on e.deptno = d.deptno
+where job = 'CLERK' and sal > 1000;
 
 -- 2. 사원 이름, 급여, 근무도시명을 조회하시오
-
+select ename, sal, loc
+from emp e
+join dept d
+on e.deptno = d.deptno;
 
 -- 3. DALLAS에 근무하는 사원 중 입사년도가 1981년도인 사원의 모든 정보 조회하시오
-
+select *
+from emp
+join dept
+using(deptno)
+where loc='DALLAS' and year(hiredate)='2010';
 
 -- 4. 이름에 'A' 문자를 포함한 사원의 사원번호, 이름, 업무, 급여, 급여등급을 조회하시오
-
+select empno, ename, job, sal, grade
+from emp
+join salgrade
+on sal between losal and hisal
+where ename like '%A%';
 
 -- 5. 사원번호, 이름, 급여, 급여 등급을 조회하시오
+select empno, ename, sal, grade
+from emp
+join salgrade
+on sal between losal and hisal;
 
 -- 6. 모든 사원번호, 사원이름, 업무, 상사번호, 상사이름
+select e.empno, e.ename, e.job, e.mgr, m.ename 
+from emp e
+left join emp m
+on e.mgr = m.empno; -- 사원의 상사번호 = 상사의 사원번호 
 
 
 -- 7. 게시글 번호, 작성자 아이디,  타이틀, 등록일, 내용,
 -- 사용자가 올린 파일 이름, 저장된 파일 이름을 추출
+select b.no, id, title, regdate, contents, rfilename, sfilename
+from board b
+left join boardfile bf
+on b.no = bf.no;
 
 -- 8. BLAKE를 관리자로 하는 사원의 모든 정보를 추출
+select * 
+from emp e, emp m
+where e.mgr = m.empno and m.ename = 'BLAKE';
 
 -- 9. 급여 등급이 1등급에 해당하는 사원의 모든 정보를 추출
+select * 
+from emp
+left join salgrade 
+on sal between losal and hisal
+where grade = 1;
 
 
 -- 10. GOODS 테이블에 등록된 총 상품 개수와 분류명별 상품 개수를 추출
-
-
-
+select  if(grouping(name)=1, '등록된 총 상품 수', 
+		ifnull(name,'미분류')) as '분류명' 
+		,count(*) "등록된 상품 수" 
+from goods g
+left join category c
+USING (cno)
+group by name with rollup ;
 
 
 
