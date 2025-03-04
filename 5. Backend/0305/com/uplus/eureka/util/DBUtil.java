62f99e807274c9bc47660926853fbc5775576531
abyss-s <lh3729@naver.com>
2025-03-05 00:05:30 +0900

    [03/05] upload files

---
+++ 5. Backend/0305/com/uplus/eureka/util/DBUtil.java
@@ -0,0 +1,14 @@
+package com.uplus.eureka.util;
+
+import org.springframework.stereotype.Component;
+
+public class DBUtil {
+	public void close(AutoCloseable... cs) {
+		try {
+			for (AutoCloseable c : cs) {
+				if(c!=null) c.close();
+			}
+		} catch (Exception e) {
+		}
+	}
+}
