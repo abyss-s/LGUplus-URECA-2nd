53750c335c5b0feb27ac7180fbe7d9899f3da740
abyss-s <lh3729@naver.com>
2025-04-14 09:51:30 +0900

    [04/14] Next.js - 수동 설치

---
+++ 8. NextJS/install-no-auto/app/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <p>Welcome to your first Next.js page.</p>
+    </div>
+  );
+}
